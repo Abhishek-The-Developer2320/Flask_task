de5cc41bbde6c34faa5d3a430e17e07d0002ef82
Abhishek Jain <abhishek.jain@zelis.com>
2025-09-19 16:01:49 +0530

    Update Flask app with deployment settings and improvements

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from myproject.extensions import db, login_manager, migrate  # Use the single db instance from extensions
-
+from dotenv import load_dotenv
+import os
+load_dotenv() 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "Hello"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/flaskdb"
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "fallback-secret")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Initialize extensions
